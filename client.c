6ae2376ec2ab8d1a6aee4da616d8e9fee0755198
waikitleung <waikitleung06@163.com>
2017-11-04 15:48:07 -0700

    modified version

--- client.c
+++ client.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unisted.h>
+#include <unistd.h>
 #include <errno.h>
 #include <string.h>
 #include <netdb.h>
@@ -13,7 +13,7 @@
 #define AWSPORT "25620"   //AWS tcp port number
 #define HOST "localhost"
  
-int data[10];
+float data[10];
 
 void *get_in_addr(struct sockaddr *sa){
 	if (sa->sa_family==AF_INET)
@@ -34,64 +34,64 @@ void data_input(){
 	}
 	//int ch;
 	while(!feof(stream)){
-		fscanf(stream,"%f",&data[0])
+		fscanf(stream,"%f",&data[0]);
 	}
 	fclose(stream);
 }
 
 
-int main(int argc, char* argv[]){
-		char function[2]
-		strcpy(function,argv[1])
-        int t=0;
-        for(t=0;t<2;t++){
-        	function[t]=toupper(function[t])
-        }
-}
+
 
 
 // setup the TCP connection
 int main(int argc,char *argv[]){
-int sockfd;
-struct addrinfo hints,*servinfo,*p;
-int rv;
+	char function[2];
+	strcpy(function,argv[1]);
+    int t=0;
+    for(t=0;t<2;t++){
+    	function[t]=toupper(function[t]);
+    }
+	int sockfd;
+	struct addrinfo hints,*servinfo,*p;
+	int rv;
 
-memset(&hints,0,sizeof hints);
-hints.ai_family=AF_UNSPEC;
-hints.ai_socktype=SOCK_STREAM;
+	memset(&hints,0,sizeof hints);
+	hints.ai_family=AF_UNSPEC;
+	hints.ai_socktype=SOCK_STREAM;
 
-if((rv=getaddrinfo(HOST,AWSPORT,&hints,&servinfo))!=0){
-	fprintf(stderr, "getaddrinfo:"%s\n",gai_strerror(rv));
-	return 1;
-}
-
-for (p=servinfo;p!=NULL;p=p->ai.next){
-	if((sockfd=socket(p->ai_family,p->ai_socktype,p->ai_protocol))==1){
-		perror("client:socket");
-		continue;
+	if((rv=getaddrinfo(HOST,AWSPORT,&hints,&servinfo))!=0){
+		fprintf(stderr, "getaddrinfo: %s\n",gai_strerror(rv));
+		return 1;
 	}
+
+	for (p=servinfo;p!=NULL;p=p->ai_next){
+		if((sockfd=socket(p->ai_family,p->ai_socktype,p->ai_protocol))==1){
+			perror("client:socket");
+			continue;
+		}
 	
-	if(connect(sockfd,p->ai_addr,p->ai_addrlen)==-1){
-		close(socket);
-		perror("client:connect")
-		continue;
+		if(connect(sockfd,p->ai_addr,p->ai_addrlen)==-1){
+			close(sockfd);
+			perror("client:connect");
+			continue;
+		}
+		break;
 	}
-	break;
+
+	if (p==NULL){
+		printf(stderr, "client:failed to connect.\n");
+		exit(0);
+		return 2;
+	}
+	freeaddrinfo(servinfo);
+	printf("The client is up and running\n");  
+	send(sockfd,function,sizeof function,0);
+	send(sockfd,(char*)&data,sizeof data,0);
+
+
+	printf("The client has sent the %s %f to AWS.\n", function,data);
+
+	int result=0;;
+	recv(sockfd,(char*)&result,sizeof result,0);
+	printf("According to AWS %s  on %f , %f",function,data,result);
 }
-
-if (p==NULL){
-	fprintf(stderr, "client:failed to connect.\n", );
-	exit(0);
-	return 2;
-}
-freeaddrinfo(servinfo);
-printf("The client is up and running\n");  
-send(sockfd,function,sizeof function,0)
-send(sockfd,(char*)&data,sizeof data,0)
-
-
-printf("The client has sent the "%s %f" to AWS.\n", function,&data);
-
-int result=0;;
-recv(sockfd,(char*)&result,sizeof result,0);
-printf("According to AWS "%s" on "%f","%f",function,&data,&result);
